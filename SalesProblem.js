fd50d4528fca00c6c51a8635a1b3ccb3491667d2
maaarcsilva3 <maaarc.silva3@gmail.com>
2023-06-23 18:30:48 +0800

    2nd commit

--- SalesProblem.js
+++ SalesProblem.js
@@ -1,59 +1,59 @@
 let productProfitArray = [
-    { product: "Product A", profit: -21 },
-    { product: "Product B", profit: 35 },
-    { product: "Product C", profit: -81 },
-    { product: "Product D", profit: -1 },
-    { product: "Product E", profit: -55 },
-    { product: "Product F", profit: -75 },
-    { product: "Product G", profit: 1 },
+  { product: "Product A", profit: -21 },
+  { product: "Product B", profit: 35 },
+  { product: "Product C", profit: -81 },
+  { product: "Product D", profit: -1 },
+  { product: "Product E", profit: -55 },
+  { product: "Product F", profit: -75 },
+  { product: "Product G", profit: 1 },
 ];
 
 window.onload = function() {
 
-    let container = document.getElementById("productContainer");
-  
-    productProfitArray.forEach(function(item) {
-      let productName = item.product;
-      let profit = item.profit;
-  
-      let pElement = document.createElement("p");
-      pElement.textContent = `${productName}: ${profit}`;
-  
-      container.appendChild(pElement);
-    });
+  let container = document.getElementById("productContainer");
+
+  productProfitArray.forEach(function(item) {
+    let productName = item.product;
+    let profit = item.profit;
+
+    let pElement = document.createElement("p");
+    pElement.textContent = `${productName}: ${profit}`;
+
+    container.appendChild(pElement);
+  });
   
 };
 
 function topProduct() {
-    if (productProfitArray.length === 0) {
-      return "No Data";
-    } else {
-      let largest = productProfitArray[0].profit;
+  if (productProfitArray.length === 0) {
+    return "No Data";
+  } else {
+    let largest = productProfitArray[0].profit;
 
-      for (let i = 1; i < productProfitArray.length; i++) {
-        if (productProfitArray[i].profit > largest) {
-          largest = productProfitArray[i].profit;
-          topProductName = productProfitArray[i].product;
-        }
+    for (let i = 1; i < productProfitArray.length; i++) {
+      if (productProfitArray[i].profit > largest) {
+        largest = productProfitArray[i].profit;
+        topProductName = productProfitArray[i].product;
       }
-      document.getElementById("display").innerText=`${topProductName}: ${largest}`;     
     }
+    document.getElementById("display").innerText=`${topProductName}: ${largest}`;     
+  }
 }
 
 function bottomProduct() {
-    if (productProfitArray.length === 0) {
-      return "No Data";
-    } else {
-      let smallest = productProfitArray[0].profit;
+  if (productProfitArray.length === 0) {
+    return "No Data";
+  } else {
+    let smallest = productProfitArray[0].profit;
 
-      for (let i = 1; i < productProfitArray.length; i++) {
-        if (productProfitArray[i].profit < smallest) {
-          smallest = productProfitArray[i].profit;
-          smallestProductName = productProfitArray[i].product;
-        }
+    for (let i = 1; i < productProfitArray.length; i++) {
+      if (productProfitArray[i].profit < smallest) {
+        smallest = productProfitArray[i].profit;
+        smallestProductName = productProfitArray[i].product;
       }
-      document.getElementById("display").innerText=`${smallestProductName}: ${smallest}`;     
     }
+    document.getElementById("display").innerText=`${smallestProductName}: ${smallest}`;     
+  }
 }
 
 function zeroProfitProduct(){
